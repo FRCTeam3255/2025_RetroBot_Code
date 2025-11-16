985809b4b46c74aff28888acc84ed049692852fa
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared-K1-10@users.noreply.github.com>
2025-11-15 16:35:55 -0800

    The funnel belt and intake
    added funnel belt and intake motors

--- src/main/java/frc/robot/constants/ConstRotors.java
+++ src/main/java/frc/robot/constants/ConstRotors.java
@@ -4,6 +4,34 @@
 
 package frc.robot.constants;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 /** Add your docs here. */
 public class ConstRotors {
+  public static final TalonFXConfiguration INTAKE_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration FUNNELBELT_CONFIGURATION = new TalonFXConfiguration();
+  static {
+
+    // TODO: replace configurations with real values
+    INTAKE_CONFIGURATION.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    INTAKE_CONFIGURATION.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    INTAKE_CONFIGURATION.CurrentLimits.SupplyCurrentLimitEnable = false;
+    INTAKE_CONFIGURATION.CurrentLimits.SupplyCurrentLowerLimit = 30;
+    INTAKE_CONFIGURATION.CurrentLimits.SupplyCurrentLimit = 60;
+    INTAKE_CONFIGURATION.CurrentLimits.SupplyCurrentLowerTime = 0.5;
+    INTAKE_CONFIGURATION.SoftwareLimitSwitch.ForwardSoftLimitEnable = false;
+    INTAKE_CONFIGURATION.SoftwareLimitSwitch.ReverseSoftLimitEnable = false;
+
+    FUNNELBELT_CONFIGURATION.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    FUNNELBELT_CONFIGURATION.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    FUNNELBELT_CONFIGURATION.CurrentLimits.SupplyCurrentLimitEnable = false;
+    FUNNELBELT_CONFIGURATION.CurrentLimits.SupplyCurrentLowerLimit = 30;
+    FUNNELBELT_CONFIGURATION.CurrentLimits.SupplyCurrentLimit = 60;
+    FUNNELBELT_CONFIGURATION.CurrentLimits.SupplyCurrentLowerTime = 0.5;
+    FUNNELBELT_CONFIGURATION.SoftwareLimitSwitch.ForwardSoftLimitEnable = false;
+    FUNNELBELT_CONFIGURATION.SoftwareLimitSwitch.ReverseSoftLimitEnable = false;
+  }
+
 }
--- src/main/java/frc/robot/subsystems/Rotors.java
+++ src/main/java/frc/robot/subsystems/Rotors.java
@@ -4,11 +4,32 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap.mapRotors;
+import frc.robot.constants.ConstRotors;
 
 public class Rotors extends SubsystemBase {
   /** Creates a new Rotors. */
-  public Rotors() {}
+  TalonFX intakeMotor;
+  TalonFX funnelbeltMotor;
+
+  public Rotors() {
+    intakeMotor = new TalonFX(mapRotors.INTAKE_CAN);
+    funnelbeltMotor = new TalonFX(mapRotors.FUNNELBELT_CAN);
+
+    intakeMotor.getConfigurator().apply(ConstRotors.INTAKE_CONFIGURATION);
+    funnelbeltMotor.getConfigurator().apply(ConstRotors.FUNNELBELT_CONFIGURATION);
+  }
+
+  public void setIntakeMotorSpeed(double speed) {
+    intakeMotor.set(speed);
+  }
+
+  public void setFunnelbeltMotorSpeed(double speed) {
+    funnelbeltMotor.set(speed);
+  }
 
   @Override
   public void periodic() {
