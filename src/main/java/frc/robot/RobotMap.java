85d465a90b01f7da813df1affd1bfeda7951f144
Krypton-6-FRCTeam3255-Shared <eliuva@icloud.com>
2025-11-21 17:30:42 -0800

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -35,7 +35,7 @@ public class RobotMap {
   public static class mapMotion {
     public static final int CLIMBER_CAN = 20;
     public static final int CONTROL_PANEL_WHEEL_CAN = 21;
-    public static final int HOOD_CAN = 21;
+    public static final int HOOD_CAN = 22;
 
   }
 
