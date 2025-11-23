288eb16edf897d045326e747e039b03d8f88d5d7
Krypton-6-FRCTeam3255-Shared <eliuva@icloud.com>
2025-11-22 18:07:10 -0800

    i fixed it taylor

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -45,7 +45,7 @@ public class RobotMap {
     public static final int INTAKE_CAN = 10;
     public static final int FUNNEL_BELT_CAN = 11;
     public static final int FLYWHEEL_CAN = 12;
-    public static final int TRANSFER_CAN = 0;
+    public static final int TRANSFER_CAN = 13;
   }
 
 }
